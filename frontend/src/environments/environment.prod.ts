0fdf87f59d6298cb40b7c86fcae9abf824822cc3
Hamlet <hmoralesluis@gmail.com>
2021-01-28 20:53:52 -0500

    Default http

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://159.203.86.45:3000',
+  apiUrl: 'http://159.203.86.45:3000',
   repoUrl: 'https://github.com/makerdao/mips/blob/master'
 };
