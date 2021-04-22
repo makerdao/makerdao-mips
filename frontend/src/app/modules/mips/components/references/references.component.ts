96f0b05d0206fdf3d732d0f4d1f0444dff98428b
Humberto <humberto.martinez@dspot.com.pl>
2021-04-22 18:21:22 -0400

    Changes on Details Sidebar section

--- frontend/src/app/modules/mips/components/references/references.component.ts
+++ frontend/src/app/modules/mips/components/references/references.component.ts
@@ -3,14 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-references',
   templateUrl: './references.component.html',
-  styleUrls: ['./references.component.scss']
+  styleUrls: ['./references.component.scss'],
 })
 export class ReferencesComponent implements OnInit {
-  @Input() references: string[];
+  @Input() references: any[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    let referencesTemp: any[] = this.references.filter((item) => {
+      return item.name !== '\n';
+    });
 
+    this.references = [...referencesTemp];
+  }
 }
