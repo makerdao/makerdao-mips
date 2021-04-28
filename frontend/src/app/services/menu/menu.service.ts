c137ad72f41b6b85a057c372f6851499f8d53b81
Humberto <humberto.martinez@dspot.com.pl>
2021-04-28 01:13:19 -0400

    fixing issues

--- frontend/src/app/services/menu/menu.service.ts
+++ frontend/src/app/services/menu/menu.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenuService {
   url: string = 'assets/data/menu.json';
+  private openedIndexChild: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
+  public openedIndexChild$: Observable<number> = this.openedIndexChild.asObservable();
 
   constructor(private http: HttpClient) {}
 
   getMenu() {
     return this.http.get(this.url);
+  }
+
+  setOpenedIndexChild(value: number) {
+    this.openedIndexChild.next(value);
   }
 }
