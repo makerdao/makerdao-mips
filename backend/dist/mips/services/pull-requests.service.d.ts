793188abe1c32f2a015b5a189753c5f6cb39177f
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2021-06-29 15:20:04 -0400

    Testing develop deploy

--- backend/dist/mips/services/pull-requests.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "mongoose";
-import { PullRequestDoc } from "../entities/pull-request.entity";
-export declare class PullRequestService {
-    private readonly pullRequestDoc;
-    constructor(pullRequestDoc: Model<PullRequestDoc>);
-    create(pullRequest: any[]): Promise<any>;
-    count(): Promise<number>;
-    aggregate(filename: string): Promise<any>;
-}
