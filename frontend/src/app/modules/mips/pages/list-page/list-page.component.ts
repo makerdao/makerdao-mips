1bc506bfbe5b3257403e67e33356b0ee89fcafbf
Diego Escobar <diego.netsolutions@gmail.com>
2021-08-11 18:30:50 -0400

    fixed repeated element on search preview

--- frontend/src/app/modules/mips/pages/list-page/list-page.component.ts
+++ frontend/src/app/modules/mips/pages/list-page/list-page.component.ts
@@ -14,10 +14,9 @@ import { MetadataShareService } from '../../services/metadata-share.service';
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
-  styleUrls: ['./list-page.component.scss']
+  styleUrls: ['./list-page.component.scss'],
 })
 export class ListPageComponent implements OnInit, AfterViewInit {
-
   mips: any = [];
   mipsAux: any = [];
   limit = 10;
@@ -33,14 +32,14 @@ export class ListPageComponent implements OnInit, AfterViewInit {
   total: number;
   moreToLoad: boolean;
   mobileSearch = false;
-  @ViewChild('filterList', {static: true}) filterList: FilterListComponent;
+  @ViewChild('filterList', { static: true }) filterList: FilterListComponent;
   showFilterList: boolean = false;
   showListSearch: boolean = false;
   listSearchMip: any[] = [];
   mipsByName: any[] = [];
   sintaxError: boolean = false;
   errorMessage: string = '';
-  defaultSearch: string = "$ and(not(@Obsolete), not(@Withdrawn))";
+  defaultSearch: string = '$ and(not(@Obsolete), not(@Withdrawn))';
   mobileView: boolean = false;
   mipsetMode: boolean = false;
 
@@ -53,18 +52,19 @@ export class ListPageComponent implements OnInit, AfterViewInit {
     private queryParamsListService: QueryParamsListService,
     private elementsRefUiService: ElementsRefUiService,
     private metadataShareService: MetadataShareService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.order = 'mip';
     this.initParametersToLoadData();
     this.searchMips();
 
-    this.footerVisibleService.isFooterVisible$.subscribe(data => {
+    this.footerVisibleService.isFooterVisible$.subscribe((data) => {
       let elementFeedback = document.getElementById('feedback');
       if (data === true && elementFeedback) {
         elementFeedback.style.position = 'relative';
-        elementFeedback.style.bottom = window.innerWidth >= 500 ? '0px' : '-10px';
+        elementFeedback.style.bottom =
+          window.innerWidth >= 500 ? '0px' : '-10px';
       } else {
         if (elementFeedback) {
           elementFeedback.style.position = 'fixed';
@@ -75,7 +75,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
 
     this.queryParamsListService.qParams$.subscribe((data: QueryParams) => {
       this.updateUrlQueryParams(data);
-    })
+    });
   }
 
   ngAfterViewInit() {
@@ -98,7 +98,8 @@ export class ListPageComponent implements OnInit, AfterViewInit {
     }, 200);
 
     this.metadataShareService.title = 'MIPs Portal';
-    this.metadataShareService.description = "Maker Improvement Proposals are the preferred mechanism for improving both Maker Governance and the Maker Protocol.";
+    this.metadataShareService.description =
+      'Maker Improvement Proposals are the preferred mechanism for improving both Maker Governance and the Maker Protocol.';
   }
 
   initParametersToLoadData() {
@@ -114,8 +115,8 @@ export class ListPageComponent implements OnInit, AfterViewInit {
     let queryParams: any = this.route.snapshot.queryParamMap;
     let status;
 
-    if (queryParams.has("status")) {
-      if (typeof queryParams.params.status === "string") {
+    if (queryParams.has('status')) {
+      if (typeof queryParams.params.status === 'string') {
         (status = []).push(queryParams.params.status);
       } else {
         status = [...queryParams.params.status];
@@ -127,7 +128,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
       search: queryParams.params.search ? queryParams.params.search : '',
       contributor: queryParams.params.contributor,
       author: queryParams.params.author,
-      mipsetMode: JSON.parse(queryParams.params.mipsetMode || null)
+      mipsetMode: JSON.parse(queryParams.params.mipsetMode || null),
     };
 
     this.queryParamsListService.queryParams = qp;
@@ -152,7 +153,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
       notcontains: [],
       equals: [],
       notequals: [],
-      inarray: []
+      inarray: [],
     };
     this.initFiltersStatus();
     this.initFilterContributor();
@@ -184,29 +185,29 @@ export class ListPageComponent implements OnInit, AfterViewInit {
   }
 
   initMipsetMode() {
-    this.mipsetMode =  this.queryParamsListService.queryParams.mipsetMode;
+    this.mipsetMode = this.queryParamsListService.queryParams.mipsetMode;
   }
 
   initFiltersStatus() {
     if (this.queryParamsListService.queryParams.status) {
       this.queryParamsListService.queryParams.status.forEach((value) => {
         switch (value) {
-          case "Accepted":
+          case 'Accepted':
             this.mipsService.setFilterArrayStatus(0, 1);
             break;
-          case "Rejected":
+          case 'Rejected':
             this.mipsService.setFilterArrayStatus(1, 1);
             break;
-          case "Archive":
+          case 'Archive':
             this.mipsService.setFilterArrayStatus(2, 1);
             break;
-          case "RFC":
+          case 'RFC':
             this.mipsService.setFilterArrayStatus(3, 1);
             break;
-          case "Obsolete":
+          case 'Obsolete':
             this.mipsService.setFilterArrayStatus(4, 1);
             break;
-          case "Formal Submission":
+          case 'Formal Submission':
             this.mipsService.setFilterArrayStatus(5, 1);
             break;
           default:
@@ -215,59 +216,111 @@ export class ListPageComponent implements OnInit, AfterViewInit {
       });
     }
 
-    this.filter.notequals.push({field: 'mip', value: -1});
+    this.filter.notequals.push({ field: 'mip', value: -1 });
 
     this.setFiltersStatus();
-
   }
 
   setFiltersStatus() {
-    let filter = {...this.filter};
+    let filter = { ...this.filter };
 
     this.filterSaved = this.mipsService.getFilter();
 
     if (this.filterSaved.arrayStatus[0] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'Accepted' });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Accepted',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Accepted'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Accepted',
+      });
     }
     if (this.filterSaved.arrayStatus[1] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'Rejected' });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Rejected',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Rejected'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Rejected',
+      });
     }
     if (this.filterSaved.arrayStatus[2] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'Archive' });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Archive',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Archive'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Archive',
+      });
     }
     if (this.filterSaved.arrayStatus[3] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'RFC' });
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: "Request for Comments (RFC)" });
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: "Request for Comments" });
+      this.pushFilterInarray(filter.inarray, { field: 'status', value: 'RFC' });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Request for Comments (RFC)',
+      });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Request for Comments',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'RFC'});
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Request for Comments (RFC)'});
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Request for Comments'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'RFC',
+      });
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Request for Comments (RFC)',
+      });
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Request for Comments',
+      });
     }
     if (this.filterSaved.arrayStatus[4] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'Obsolete' });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Obsolete',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Obsolete'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Obsolete',
+      });
     }
     if (this.filterSaved.arrayStatus[5] === 1) {
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: 'Formal Submission' });
-      this.pushFilterInarray(filter.inarray, {field: 'status', value: "Formal Submission (FS)" });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Formal Submission',
+      });
+      this.pushFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Formal Submission (FS)',
+      });
     } else {
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Formal Submission'});
-      this.deleteFilterInarray(filter.inarray, {field: 'status', value: 'Formal Submission (FS)'});
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Formal Submission',
+      });
+      this.deleteFilterInarray(filter.inarray, {
+        field: 'status',
+        value: 'Formal Submission (FS)',
+      });
     }
 
-    this.filter = {...filter};
+    this.filter = { ...filter };
   }
 
   pushFilterInarray(array: Array<any>, data: any) {
-    let item = array.find(i => i.field === data.field && i.value === data.value);
+    let item = array.find(
+      (i) => i.field === data.field && i.value === data.value
+    );
 
     if (!item) {
       array.push(data);
@@ -275,7 +328,9 @@ export class ListPageComponent implements OnInit, AfterViewInit {
   }
 
   deleteFilterInarray(array: Array<any>, data: any) {
-    let index = array.findIndex(i => i.field === data.field && i.value === data.value);
+    let index = array.findIndex(
+      (i) => i.field === data.field && i.value === data.value
+    );
 
     if (index !== -1) {
       array.splice(index, 1);
@@ -283,15 +338,18 @@ export class ListPageComponent implements OnInit, AfterViewInit {
   }
 
   searchMips(): void {
-    let index = this.filter.equals.findIndex(item => item.field === 'proposal');
+    let index = this.filter.equals.findIndex(
+      (item) => item.field === 'proposal'
+    );
 
-    if (this.filterOrSearch()) {  // filter or search
+    if (this.filterOrSearch()) {
+      // filter or search
       if (index !== -1) {
-        this.filter.equals.splice(index, 1);  // include subproposals in searching
+        this.filter.equals.splice(index, 1); // include subproposals in searching
       }
     } else {
       if (index === -1) {
-        this.filter.equals.push({field: 'proposal', value: ""});  // no subproposals
+        this.filter.equals.push({ field: 'proposal', value: '' }); // no subproposals
       }
     }
 
@@ -377,27 +435,35 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         filter,
         'title proposal mipName filename paragraphSummary sentenceSummary mip status mipFather'
       )
-    .subscribe(data => {
-      this.mipsByName = data.items;
+      .subscribe((data) => {
+        this.mipsByName = data.items;
 
-      this.showListSearch = true;
-      this.listSearchMip = this.mipsByName.map(item => {
-        return {
-          content: item.mipName + " " + (item.title !== undefined ? item.title : ""),
-          mipName: item.mipName,
-          id: item._id
-        }
+        this.showListSearch = true;
+        this.listSearchMip = this.mipsByName.map((item) => {
+          const cleanedTitle = item.title.replace(/[^\w]*/g, '');
+          const cleanedMipName = item.mipName.replace(/[^\w]*/g, '');
+
+          const titleContainsMipsName = cleanedTitle.includes(cleanedMipName);
+
+          return {
+            content:
+              (titleContainsMipsName ? '' : item.mipName) +
+              ' ' +
+              (item.title !== undefined ? item.title : ''),
+            mipName: item.mipName,
+            id: item._id,
+          };
+        });
       });
-    });
-}
+  }
 
   onSendPagination(): void {
-      this.loadingPlus = true;
-      this.page++;
-      this.limitAux += 10;
-      if (this.moreToLoad) {
-        this.searchMips();
-      }
+    this.loadingPlus = true;
+    this.page++;
+    this.limitAux += 10;
+    if (this.moreToLoad) {
+      this.searchMips();
+    }
   }
 
   onSendFilters() {
@@ -419,7 +485,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
       let filter = {
         contains: [],
       };
-      filter.contains.push({field: 'mipName', value: event.target.value});
+      filter.contains.push({ field: 'mipName', value: event.target.value });
       this.searchMipsByName(0, 0, 'mipName', '', filter);
       this.limit = 0;
     } else {
@@ -465,7 +531,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
   }
 
   goToMipDetails(name) {
-    this.router.navigate(["/mips/details/", name]);
+    this.router.navigate(['/mips/details/', name]);
   }
 
   initFiltersList(): void {
@@ -478,7 +544,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '0',
         text: 'accepted',
         value: '0',
-        color: '#27AE60'
+        color: '#27AE60',
       });
     }
     if (filterSaved.arrayStatus[1] === 1) {
@@ -486,7 +552,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '1',
         text: 'rejected',
         value: '1',
-        color: '#EB5757'
+        color: '#EB5757',
       });
     }
     if (filterSaved.arrayStatus[2] === 1) {
@@ -494,7 +560,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '2',
         text: 'archive',
         value: '2',
-        color: '#748AA1'
+        color: '#748AA1',
       });
     }
     if (filterSaved.arrayStatus[3] === 1) {
@@ -502,7 +568,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '3',
         text: 'rfc',
         value: '3',
-        color: '#F2994A'
+        color: '#F2994A',
       });
     }
     if (filterSaved.arrayStatus[4] === 1) {
@@ -510,7 +576,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '4',
         text: 'obsolete',
         value: '4',
-        color: '#B5B12A'
+        color: '#B5B12A',
       });
     }
     if (filterSaved.arrayStatus[5] === 1) {
@@ -518,7 +584,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
         id: '5',
         text: 'formal submission',
         value: '5',
-        color: '#78288C'
+        color: '#78288C',
       });
     }
   }
@@ -531,7 +597,7 @@ export class ListPageComponent implements OnInit, AfterViewInit {
     let qp: QueryParams = {
       status: [],
       search: this.search,
-      mipsetMode: this.mipsetMode
+      mipsetMode: this.mipsetMode,
     };
 
     if (filterSaved.arrayStatus[0] === 1) {
@@ -558,10 +624,10 @@ export class ListPageComponent implements OnInit, AfterViewInit {
 
   updateUrlQueryParams(qp: QueryParams) {
     let navigationExtras: NavigationExtras = {
-      queryParams: qp
-    }
+      queryParams: qp,
+    };
 
-    this.router.navigate(['/mips/list'], {...navigationExtras});
+    this.router.navigate(['/mips/list'], { ...navigationExtras });
   }
 
   onCheckedMipsetMode(ev) {
