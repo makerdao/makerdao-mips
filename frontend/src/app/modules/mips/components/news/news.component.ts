fe3025274ea6f014f3ce6a82400d253dea68fec5
Yasmany Santalla Pereda <ysantalla88@gmail.com>
2021-10-28 12:23:38 -0400

    Frontend app: Adding support to notifications

--- frontend/src/app/modules/mips/components/news/news.component.ts
+++ frontend/src/app/modules/mips/components/news/news.component.ts
@@ -1,5 +1,8 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { DarkModeService } from 'src/app/services/dark-mode/dark-mode.service';
 import { NewsService } from 'src/app/services/news/news.service';
 
 @Component({
@@ -11,8 +14,17 @@ export class NewsComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
   news: any[] = [];
 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
   constructor(
-    private newsService: NewsService
+    private newsService: NewsService,
+    public darkModeService: DarkModeService,
+    private breakpointObserver: BreakpointObserver,
   ) {}
 
   ngOnInit(): void {
