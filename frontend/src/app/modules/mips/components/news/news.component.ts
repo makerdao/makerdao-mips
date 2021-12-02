149973f93adf7391c26f320666265a2ef7eec4ac
Diego Escobar <diego.netsolutions@gmail.com>
2021-12-02 15:23:28 +0300

    implemented auto re open hiden news

--- frontend/src/app/modules/mips/components/news/news.component.ts
+++ frontend/src/app/modules/mips/components/news/news.component.ts
@@ -6,51 +6,55 @@ import { DarkModeService } from 'src/app/services/dark-mode/dark-mode.service';
 import { NewsService } from 'src/app/services/news/news.service';
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss'],
+    selector: 'app-news',
+    templateUrl: './news.component.html',
+    styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit, OnDestroy {
-  subscriptions: Subscription[] = [];
-  news: any[] = [];
+    subscriptions: Subscription[] = [];
+    news: any[] = [];
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
+    isHandset$: Observable<boolean> = this.breakpointObserver
+        .observe(Breakpoints.Handset)
+        .pipe(
+            map((result) => result.matches),
+            shareReplay()
+        );
 
-  constructor(
-    private newsService: NewsService,
-    public darkModeService: DarkModeService,
-    private breakpointObserver: BreakpointObserver,
-  ) {}
+    constructor(
+        private newsService: NewsService,
+        public darkModeService: DarkModeService,
+        private breakpointObserver: BreakpointObserver,
+    ) {}
 
-  ngOnInit(): void {
-    this.subscriptions.push(
-      this.newsService.getData().subscribe((data) => {
-        this.news = data;
-      })
-    );
-  }
-
-  removeItem(i: number, id: string): void {
-    this.news = this.news.filter((v, index) => index !== i);
-
-    const oldNews: string[] = JSON.parse(localStorage.getItem('OLD-NEWS'));
-
-    if (oldNews) {
-      localStorage.setItem('OLD-NEWS', JSON.stringify([...oldNews, id]));
-    } else {
-      localStorage.setItem('OLD-NEWS', JSON.stringify([id]));
+    ngOnInit(): void {
+        this.subscriptions.push(
+            this.newsService.getData().subscribe((data) => {
+                this.news = data;
+            })
+        );
     }
 
-  }
+    removeItem(i: number, id: string): void {
+        this.news = this.news.filter((v, index) => index !== i);
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((s) => {
-      s.unsubscribe();
-    });
-  }
+        const oldNews: any[] = JSON.parse(localStorage.getItem('OLD-NEWS')) || [];
+
+        const cleanedOldNews = oldNews.filter(item => item.id !== id);
+        // Avoiding repeated ID problem on update same id data
+
+        const elementToRemove = {
+            id,
+            timeClosed: Date.now()
+        };
+
+        localStorage.setItem('OLD-NEWS', JSON.stringify([...cleanedOldNews, elementToRemove]));
+
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach((s) => {
+            s.unsubscribe();
+        });
+    }
 }
