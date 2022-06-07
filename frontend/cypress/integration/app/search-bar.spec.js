c41c94204f5eb63eaa908c3c159f0ecca1f89c94
Reynier <raalarcon9705@gmail.com>
2022-06-07 14:34:27 -0400

    simple query testusing cypress

--- frontend/cypress/integration/app/search-bar.spec.js
+++ frontend/cypress/integration/app/search-bar.spec.js
@@ -1,15 +1,43 @@
 /// <reference types="cypress" />
 
-const simpleSearch = 'options';
-const mipSearch = 'MIP32';
-const dollarSearch = '$ @ACCEPTED';
+const simpleSearch = "options";
+const mipSearch = "MIP32";
+const dollarSearch = "$ @ACCEPTED";
 
 describe("Search Bar functionality", () => {
   beforeEach(() => {
-    cy.visit('')
+    cy.visit("");
   });
 
-  it('Simple query', () => {
-    cy.get('[data-cy=search-input]').click().type(simpleSearch);
+  it('The results of simple queries must contain the entered text', () => {
+    cy.get("[data-cy=search-input]")
+    .click()
+    .type(simpleSearch)
+    .type('{enter}')
+    .get("[data-cy=hide-parent-checkbox] label")
+    .click();
+
+    cy.get('tr[data-cy=search-result]')
+      .each((_, index) => {
+        cy.get('tr[data-cy=search-result]')
+          .eq(index)
+          .click()
+          .get('[data-cy=mip-content]')
+          .contains(simpleSearch, { matchCase: false })
+          .should('exist')
+          .go('back');
+      });
+
+
+      cy.get('[data-cy=subporposal-item]')
+        .each((_, index) => {
+          cy.get('[data-cy=subporposal-item]')
+            .eq(index)
+            .click()
+            .get('[data-cy=mip-content]')
+            .contains(simpleSearch, { matchCase: false })
+            .should('exist')
+            .go('back');
+        })
   })
 });
