8649c6d447f63ddd30a7c143229787c7a6111225
RoxanaSocaDspot <roxana.soca@dspot.com.pl>
2022-06-07 11:13:26 +0300

    fix: adding await

--- backend/src/mips/services/github.service.ts
+++ backend/src/mips/services/github.service.ts
@@ -36,11 +36,12 @@ export class GithubService {
 
   async pullRequests(pullRequests: RequestDocument, after?: string): Promise<any> {
     this.logger.error('Starting request process for pull requests: ' + this.githubRepository+ ' ' + this.githubRepositoryOwner);
-    const result = this.graphQLClient.request(pullRequests, {
+    const result = await this.graphQLClient.request(pullRequests, {
       name: this.githubRepository,
       owner: this.githubRepositoryOwner,
       after,
     });
+    this.logger.error(result);
     this.logger.error('Finished request process for pull requests');
     return result;
   }
