b7cc56b8486faeb77f6cac84eb4b715d06b75c16
RoxanaSocaDspot <roxana.soca@dspot.com.pl>
2022-06-07 16:03:14 +0300

    feat: adding integrations test for smartSearch

--- backend/test/integration/mips.controller.spec.ts
+++ backend/test/integration/mips.controller.spec.ts
@@ -1,11 +1,12 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { Language } from "../../src/mips/entities/mips.entity";
+import { Language, MIP } from "../../src/mips/entities/mips.entity";
 import { MIPsController } from "../../src/mips/mips.controller";
 import { MIPsModule } from "../../src/mips/mips.module";
-import { mipData, mipData_2, mipNumber_1, mipNumber_2 } from "../../src/mips/services/data-test/data";
+import { mipData, mipData_2, mipNumber_1, mipNumber_2, smartSearchFieldMock, statusResultMock, tagResultMock } from "../../src/mips/services/data-test/data";
 import { MIPsService } from "../../src/mips/services/mips.service";
 const faker = require("faker");
 
@@ -175,6 +176,107 @@ describe('MIPsController', () => {
         expect(mip).toBeDefined();
         expect(mip.length).toEqual(1);
         expect(mip).toEqual([expectedMip_2]);
+      });
+
+      afterEach(async () => {
+        await mipsService.update(mip_1._id, {
+          ...mip_1,
+          language: Language.English,
+        });
+        const { items: mips } = await mipsService.findAll({ limit: 10, page: 0 });
+        await mipsService.deleteManyByIds(
+          mips.map((mip) => mip._id)
+        );
+      });
+    });
+  });
+
+  describe('smartSearch', () => {
+    it('shoud return an array with no mips by tags', async () => {
+      const mips = await controller.smartSearch('tags', '');
+
+      expect(mips).toBeDefined();
+      expect(mips.length).toEqual(0);
+    });
+
+    it('shoud return an array with no mips by status', async () => {
+      const mips = await controller.smartSearch('status', '');
+
+      expect(mips).toBeDefined();
+      expect(mips.length).toEqual(0);
+    });
+
+    it('shoud throw an error by incorrect field name', async () => {
+      try {
+        await controller.smartSearch(smartSearchFieldMock, '');
+      } catch (error) {
+        expect(error).toEqual(new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: `Field ${smartSearchFieldMock} invalid`,
+          },
+          HttpStatus.BAD_REQUEST
+        ));
+      }
+    });
+
+    describe('existing data', () => {
+      let mip_1;
+      let mip_2;
+      let expectedMip_1;
+      let expectedMip_2;
+
+      beforeEach(async () => {
+        mip_1 = await mipsService.create({
+          ...mipData,
+          language: Language.Spanish,
+        });
+        expectedMip_1 = {
+          ...mipData,
+          _id: mip_1._id,
+        };
+        delete expectedMip_1.file;
+        delete expectedMip_1.sectionsRaw;
+        mip_2 = await mipsService.create({
+          ...mipData_2,
+          language: Language.English,
+        });
+        expectedMip_2 = {
+          ...mipData_2,
+          _id: mip_2._id,
+        };
+        delete expectedMip_2.file;
+        delete expectedMip_2.sectionsRaw;
+      });
+
+      it('should return an emty array by tag in wrong language', async () => {
+        const mips = await controller.smartSearch('tags', mip_1.tags[0], Language.English);
+
+        expect(mips).toBeDefined();
+        expect(mips.length).toEqual(0);
+      });
+
+      it('should return an emty array by status in wrong language', async () => {
+        const mips = await controller.smartSearch('status', mip_2.status, Language.Spanish);
+
+        expect(mips).toBeDefined();
+        expect(mips.length).toEqual(0);
+      });
+
+      it('should return one mip by tag', async () => {
+        const mips = await controller.smartSearch('tags', mip_1.tags[0], Language.Spanish);
+
+        expect(mips).toBeDefined();
+        expect(mips.length).toEqual(1);
+        expect(mips[0]).toEqual(tagResultMock);
+      });
+
+      it('should return one mip by status', async () => {
+        const mips = await controller.smartSearch('status', mip_2.status, Language.English);
+
+        expect(mips).toBeDefined();
+        expect(mips.length).toEqual(1);
+        expect(mips[0]).toEqual(statusResultMock);
       });
 
       afterEach(async () => {
