7d47384f8ca1662d03a2d0b90d9d17ccef72302e
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-08 10:06:14 +0300

    Searching inside internal view.

--- frontend/cypress/integration/app/regular-search.spec.js
+++ frontend/cypress/integration/app/regular-search.spec.js
@@ -1,16 +1,34 @@
-describe('Test Regular Search',()=>{
-    beforeEach(()=>{
-        cy.visit('')
-    })
-    
-    it('should find MIps containing giving keywords',()=>{
-        cy.get('[data-cy=search-input]').type('proposal')
-        cy.get('[data-cy=search-input]').type('{enter}')
+describe('Test Regular Search', () => {
+  beforeEach(() => {
+    cy.visit('')
+  })
 
-        cy.get('[data-cy=table-list-mips]').should('be.visible')
+  it('should find MIps containing giving keywords', () => {
+    const keywords = ["proposal", "admin"];
 
-        cy.get('[data-cy=table-list-mips] tr.maker-element-row').each(($row)=>{
-            cy.wrap($row).should('contain.text','roposal')          
-        })        
+    keywords.forEach($word => {
+      cy.visit('')
+
+      cy.get('[data-cy=search-input]').type($word)
+      cy.get('[data-cy=search-input]').type('{enter}')
+      cy.get('[data-cy=table-list-mips]').should('be.visible')
+
+      const rows = []
+
+      cy.get('[data-cy=table-list-mips] tr.maker-element-row').each(($row) => {
+        cy.wrap($row).find("a").then($link => {
+          if (Cypress.$($link).hasClass('mipTitleList')){
+            const $href = Cypress.$($link).attr("href");
+            rows.push($href);
+          }
+        });
+      })
+
+      cy.wrap(rows).each($row => {
+        // console.log($row);
+        cy.visit($row);
+        cy.get(".row.row-tree-column").should("contain.text", $word.substr(1));
+      })
     })
+  })
 })
