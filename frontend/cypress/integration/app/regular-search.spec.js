aafc9e2afaf4a3ba575784d7246d624a9f22db5f
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-08 12:02:25 +0300

    Completed test for regular search and pattern MIP#

--- frontend/cypress/integration/app/regular-search.spec.js
+++ frontend/cypress/integration/app/regular-search.spec.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 describe('Test Regular Search', () => {
   beforeEach(() => {
     cy.visit('')
   })
 
   it('should find MIps containing giving keywords', () => {
-    const keywords = ["proposal", "admin"];
+    const keywords = ["proposal"];
 
     keywords.forEach($word => {
       cy.visit('')
@@ -17,51 +16,37 @@ describe('Test Regular Search', () => {
       const rows = []
 
       cy.get('[data-cy=table-list-mips] tr.maker-element-row').each(($row) => {
-        cy.wrap($row).find("a").then($link => {
-          if (Cypress.$($link).hasClass('mipTitleList')){
-            const $href = Cypress.$($link).attr("href");
-            rows.push($href);
-          }
+
+          cy.wrap($row).find("a").then($link => {
+            if (Cypress.$($link).hasClass('mipTitleList')) {
+              const $href = Cypress.$($link).attr("href");
+              rows.push($href);
+            }
+          });
+      });
+
+        cy.wrap(rows).each($row => {
+          cy.visit($row);
+          cy.get(".row.row-tree-column").should("contain.text", $word.substr(1));
         });
       })
+    });
 
-      cy.wrap(rows).each($row => {
-        // console.log($row);
-        cy.visit($row);
-        cy.get(".row.row-tree-column").should("contain.text", $word.substr(1));
-=======
-describe('Test Regular Search',()=>{
-    beforeEach(()=>{
-        cy.visit('')
-    })
+  it('should find MIps with the pattern MIP#', () => {
+    const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-    // it('should find MIps containing giving keywords',()=>{
-    //     cy.get('[data-cy=search-input]').type('proposal')
-    //     cy.get('[data-cy=search-input]').type('{enter}')
-    //
-    //     cy.get('[data-cy=table-list-mips]').should('be.visible')
-    //
-    //     cy.get('[data-cy=table-list-mips] tr.maker-element-row').each(($row)=>{
-    //         cy.wrap($row).should('contain.text','roposal')
-    //     })
-    // })
-
-  it('should find MIps with the pattern MIP#',()=>{
-    const values =['1','2','3','4','5','6','7','8','9','10']
-
-    values.forEach(value=>{
+    values.forEach(value => {
       cy.visit('')
       cy.get('[data-cy=search-input]').clear()
-      cy.get('[data-cy=search-input]').type('MIP'+value)
+      cy.get('[data-cy=search-input]').type('MIP' + value)
       cy.get('[data-cy=search-input]').type('{enter}')
 
-      cy.get('[data-cy=table-list-mips] tr.maker-element-row td.mat-column-pos').each(($col)=>{
-        cy.wrap($col).invoke('text').invoke('trim').should($text=>{
-          if ($text !== '' && $text !== '-1'){
+      cy.get('[data-cy=table-list-mips] tr.maker-element-row td.mat-column-pos').each(($col) => {
+        cy.wrap($col).invoke('text').invoke('trim').should($text => {
+          if ($text !== '' && $text !== '-1') {
             expect($text).to.contain(value);
           }
         })
->>>>>>> 0590c7546ea0179064df3e79407a0fa856b1389c
       })
     })
   })
