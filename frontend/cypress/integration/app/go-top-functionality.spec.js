16e66584cf1d98614781859df465ea2d70b28aa0
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-09 16:37:45 +0300

    Go Top functionality

--- frontend/cypress/integration/app/go-top-functionality.spec.js
+++ frontend/cypress/integration/app/go-top-functionality.spec.js
@@ -14,7 +14,7 @@
 describe('Go Top Functionaliy', () => {
     beforeEach(() => {
        cy.visit('')
-    }) 
+    })
 
     it('shows and hide Go Top component', () => {
         cy.get("[data-cy=button-go-top]").should('not.be.visible');
@@ -23,7 +23,7 @@ describe('Go Top Functionaliy', () => {
         cy.get("[data-cy=button-go-top]").click();
         cy.get("[data-cy=button-go-top]").should('be.visible');
         cy.scrollTo(0, 0);
+
         cy.get("[data-cy=button-go-top]").should('not.be.visible');
       })
   })
-  
