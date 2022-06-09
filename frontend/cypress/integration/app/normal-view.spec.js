7990bf10c15e5e71df34202b9cb65b72f4410088
Reynier <raalarcon9705@gmail.com>
2022-06-09 09:37:11 -0400

    Testing columns and initial sort

---
+++ frontend/cypress/integration/app/normal-view.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('Normal View', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('Should have all columns', () => {
+    const columns = ['#', 'title', 'summary', 'status', 'links'];
+
+    cy.get('[data-cy=table-list-mips] > thead > tr > th').each(($th, idx) => {
+      cy.wrap($th).invoke('text').invoke('trim').should('eq', columns[idx])
+    })
+  });
+
+  it('Initially should be sorted by #', () => {
+    const columns = ['#', 'title', 'summary', 'status', 'links'];
+
+    let prevNo = -1;
+
+    cy.get('[data-cy=table-list-mips] > tbody > tr[data-cy=search-result] > td:first-child').each(($td, idx) => {
+      const curNo = parseInt($td.text().trim());
+      cy.wrap(curNo).should('be.gt', prevNo);
+      prevNo = curNo;
+    })
+  });
+});
