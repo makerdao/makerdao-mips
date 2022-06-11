4551b3dad413dd0f32039b701b104a998505ab8f
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-11 09:54:11 +0300

    Testing news interactions

---
+++ frontend/cypress/integration/app/news-interactions.spec.js
@@ -0,0 +1,27 @@
+describe('Test Regular Search', () => {
+  beforeEach(() => {
+    cy.visit('')
+  })
+
+  it('should render the news UI above the page',()=>{
+      cy.get('app-list-page app-news').should('be.visible')
+
+    cy.get('app-list-page app-news').find('.container').each($container=>{
+      if (Cypress.$($container).hasClass('container-green')){
+        cy.wrap($container).find('.item-icon-green').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-yellow')){
+        cy.wrap($container).find('.item-icon-yellow').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-red')){
+        cy.wrap($container).find('.item-icon-red').should('exist')
+      }
+
+      cy.wrap($container).find('.item-title').should('be.visible')
+      cy.wrap($container).find('.item-description').should('be.visible')
+
+    })
+  })
+})
