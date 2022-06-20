666d76df8c9496eef0dfdd11ff5c764fd4083129
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-20 14:52:37 +0300

    Added test in dark mode.

--- frontend/cypress/integration/app/news-interactions.spec.js
+++ frontend/cypress/integration/app/news-interactions.spec.js
@@ -24,4 +24,27 @@ describe('Test News Interactions', () => {
 
     })
   })
+
+  it('should render the news UI above the page on dark modw',()=>{
+    cy.get('div.darkModeToggler').click()
+    cy.get('app-list-page app-news').should('be.visible')
+
+    cy.get('app-list-page app-news').find('.container-dark').each($container=>{
+      if (Cypress.$($container).hasClass('container-green')){
+        cy.wrap($container).find('.item-icon-green').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-yellow')){
+        cy.wrap($container).find('.item-icon-yellow').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-red')){
+        cy.wrap($container).find('.item-icon-red').should('exist')
+      }
+
+      cy.wrap($container).find('.item-title').should('be.visible')
+      cy.wrap($container).find('.item-description-dark').should('be.visible')
+
+    })
+  })
 })
