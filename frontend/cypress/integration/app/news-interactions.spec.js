6b7eb4743393c53a6015f5acd03ac8b8ef6aa618
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-20 14:56:49 +0300

    Added tests in spanish in light and dark modes.

--- frontend/cypress/integration/app/news-interactions.spec.js
+++ frontend/cypress/integration/app/news-interactions.spec.js
@@ -47,4 +47,56 @@ describe('Test News Interactions', () => {
 
     })
   })
+
+  it('should render the news UI above the page on Spanish language',()=>{
+    cy.get('a.language-menu').click()
+    cy.get('div.language-menu').find('app-menu').eq(0).click()
+
+    cy.get('app-list-page app-news').should('be.visible')
+
+    cy.get('app-list-page app-news').find('.container').each($container=>{
+      if (Cypress.$($container).hasClass('container-green')){
+        cy.wrap($container).find('.item-icon-green').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-yellow')){
+        cy.wrap($container).find('.item-icon-yellow').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-red')){
+        cy.wrap($container).find('.item-icon-red').should('exist')
+      }
+
+      cy.wrap($container).find('.item-title').should('be.visible')
+      cy.wrap($container).find('.item-description').should('be.visible')
+
+    })
+  })
+
+  it('should render the news UI above the page on dark modw with spanish language',()=>{
+    cy.get('a.language-menu').click()
+    cy.get('div.language-menu').find('app-menu').eq(0).click()
+
+    cy.get('div.darkModeToggler').click()
+    cy.get('app-list-page app-news').should('be.visible')
+
+    cy.get('app-list-page app-news').find('.container-dark').each($container=>{
+      if (Cypress.$($container).hasClass('container-green')){
+        cy.wrap($container).find('.item-icon-green').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-yellow')){
+        cy.wrap($container).find('.item-icon-yellow').should('exist')
+      }
+
+      if (Cypress.$($container).hasClass('container-red')){
+        cy.wrap($container).find('.item-icon-red').should('exist')
+      }
+
+      cy.wrap($container).find('.item-title').should('be.visible')
+      cy.wrap($container).find('.item-description-dark').should('be.visible')
+
+    })
+  })
+
 })
