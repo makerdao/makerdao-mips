54afcaf389c83735e33cab3cb5b74a3ce8685cd7
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-22 09:49:02 +0300

    Added test case for dark mode.

--- frontend/cypress/integration/app/md-viewer.spec.js
+++ frontend/cypress/integration/app/md-viewer.spec.js
@@ -3,12 +3,25 @@ describe('Test MD Viewer', () => {
     cy.visit('/mips/md-viewer?mdUrl=https:%2F%2Fraw.githubusercontent.com%2Fmakerdao%2Fmips%2Fmaster%2FMIP1%2FMIP1c4-Subproposal-Template.md')
   })
 
-  it('should find MIps containing giving keywords', () => {
+  it('should open box container', () => {
       cy.get('app-proposal-components').should('exist')
 
       cy.get('app-proposal-components .container .content div').each($div=>{
        cy.wrap($div).click({force:true})
        cy.wait(2000)
+    })
+
+    cy.get('app-detail-content div.container').scrollTo('bottom',{ensureScrollable:false})
+  });
+
+  it('should open box container on darkmode', () => {
+    cy.get('div.darkModeToggler').click()
+
+    cy.get('app-proposal-components').should('exist')
+
+    cy.get('app-proposal-components .container .content div').each($div=>{
+      cy.wrap($div).click({force:true})
+      cy.wait(2000)
     })
 
     cy.get('app-detail-content div.container').scrollTo('bottom',{ensureScrollable:false})
