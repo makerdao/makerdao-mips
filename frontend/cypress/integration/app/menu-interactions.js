2af36536d506859f829365d138e541a794dc4553
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-22 08:24:48 +0300

    Added test case for dark mode.

--- frontend/cypress/integration/app/menu-interactions.js
+++ frontend/cypress/integration/app/menu-interactions.js
@@ -18,4 +18,38 @@ describe('Test Menu Interactions', () => {
       })
     })
   })
+
+  it("should render submenus on darkmode",()=>{
+    cy.get('div.darkModeToggler').click()
+
+    const menuHeaders =['Learn','Views','Get in Touch'];
+
+    menuHeaders.forEach($header=>{
+      cy.visit('')
+      cy.get('app-nav-menu div').contains($header).click()
+      cy.get('.dropdown-content-first-level').should('be.visible')
+      cy.get('.dropdown-content-first-level a').each(($a,$idx)=>{
+        cy.get('.dropdown-content-first-level a').eq($idx).click()
+        cy.wait(2000)
+        cy.visit('')
+        cy.get('app-nav-menu div').contains($header).click()
+      })
+    })
+  })
+
+  it("should render submenus on spanish",()=>{
+    const menuHeaders =['Aprende','Vistas','Ponte en Contacto'];
+
+    menuHeaders.forEach($header=>{
+      cy.visit('')
+      cy.get('app-nav-menu div').contains($header).click()
+      cy.get('.dropdown-content-first-level').should('be.visible')
+      cy.get('.dropdown-content-first-level a').each(($a,$idx)=>{
+        cy.get('.dropdown-content-first-level a').eq($idx).click()
+        cy.wait(2000)
+        cy.visit('')
+        cy.get('app-nav-menu div').contains($header).click()
+      })
+    })
+  })
 })
