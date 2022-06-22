fdfef3384e72e0d695f40a5d8ca9bbcb78134f8d
Anibal Sanchez <anibalnuma@gmail.com>
2022-06-22 11:37:55 +0300

    Adding check for location changing when clicking the headings to the left.

--- frontend/cypress/integration/app/md-viewer.spec.js
+++ frontend/cypress/integration/app/md-viewer.spec.js
@@ -4,18 +4,22 @@ describe('Test MD Viewer', () => {
   })
 
   it('should open box container', () => {
-      cy.get('app-proposal-components').should('exist')
+    cy.get('app-proposal-components').should('exist')
 
-      cy.get('app-proposal-components .container .content div').each(($div,$idx)=>{
-        cy.get('app-proposal-components .container .content div').eq($idx).click()
+    cy.get('app-proposal-components .container .content > div').its('length').then($amount=>{
+      for (let c =0 ; c < $amount; c++){
+        cy.visit('/mips/md-viewer?mdUrl=https:%2F%2Fraw.githubusercontent.com%2Fmakerdao%2Fmips%2Fmaster%2FMIP1%2FMIP1c4-Subproposal-Template.md')
+        cy.get('app-proposal-components .container .content > div').eq(c).click()
         cy.wait(2000)
         cy.location().then($loc=>{
-          cy.get('app-proposal-components .container .content div').eq($idx).invoke('text').invoke('trim').should('contain',$loc.hash.substring(1))
-          cy.visit('/mips/md-viewer?mdUrl=https:%2F%2Fraw.githubusercontent.com%2Fmakerdao%2Fmips%2Fmaster%2FMIP1%2FMIP1c4-Subproposal-Template.md')
-       })
+          cy.get('app-proposal-components .container .content > div').eq(c).invoke('text').invoke('trim').should('contain',$loc.hash.substring(2))
+          cy.wait(4000)
+        })
+        console.log('CORRI ',c)
+      }
     })
 
-    cy.get('app-detail-content div.container').scrollTo('bottom',{ensureScrollable:false})
+     cy.get('app-detail-content div.container').scrollTo('bottom',{ensureScrollable:false})
   });
 
   // it('should open box container on darkmode', () => {
