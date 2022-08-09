ea2ee4603f3c65fd9accf10c8e6c716893c53411
Daniel Vega <dvegap95@gmail.com>
2022-08-08 23:34:17 -0400

    testing normal view subproposals open on MIPS where they are present

--- frontend/cypress/integration/cucumber/normal-view-part-2.feature
+++ frontend/cypress/integration/cucumber/normal-view-part-2.feature
@@ -1,27 +1,76 @@
 Feature: Normal view part 2
 
- Scenario: Component opens on MIPS where they are present
+# Related to MIP's
+#  Default
+ Scenario: Mip's link navigates to right page
     Given The user opens the main page
     When The user clicks the row corresponding to MIP "6"
     Then The view page corresponding to MIP "6" should open
     And The open page should contain the title of MIP "6"
-
- Scenario: Component opens on MIPS where they are present (Dark mode)
+#  Dark mode
+ Scenario: Mip's link navigates to right page (Dark mode)
     Given The user opens the main page
     And Dark mode is toggled
     When The user clicks the row corresponding to MIP "6"
     Then The view page corresponding to MIP "6" should open
     And The open page should contain the title of MIP "6"
-
- Scenario: Component opens on MIPS where they are present (English)
+#  Spanish
+ Scenario: Mip's link navigates to right page (Spanish)
     Given The user opens the main page
-    And The user selects "English" language
+    And The user selects "Spanish" language
     When The user clicks the row corresponding to MIP "6"
     Then The view page corresponding to MIP "6" should open
     And The open page should contain the title of MIP "6"
 
+# Related to Components
+#  Default
+ Scenario: Component opens on MIPS where they are present
+    Given The user opens the main page
+    When The user clicks dropdown icon on the row corresponding to MIP "10"
+    Then The component "3" belonging to MIP "10" should appear in the row right below it
+#  Dark mode
+ Scenario: Component opens on MIPS where they are present (Dark mode)
+    Given The user opens the main page
+    And Dark mode is toggled
+    When The user clicks dropdown icon on the row corresponding to MIP "10"
+    Then The component "3" belonging to MIP "10" should appear in the row right below it
+#  Spanish
+ Scenario: Component opens on MIPS where they are present (Spanish)
+    Given The user opens the main page
+    And The user selects "Spanish" language
+    When The user clicks dropdown icon on the row corresponding to MIP "10"
+    Then The component "3" belonging to MIP "10" should appear in the row right below it
 
-#  Scenario: Component opens on MIPS where they are present
-#     Given The user opens the main page
-#     When The user clicks a MIP with "2"
-#     Then The corresponding MIP view should open in "2"
+
+# Related to subproposals
+#  Default
+ Scenario: SubProposals open on MIPS where they are present
+    Given The user opens the main page
+    When The user clicks dropdown icon on the row corresponding to MIP "4"
+    And The user opens the sub-proposals menu of component "2" of MIP "4"
+    Then The list of subproposals belonging to component "2" of MIP "4" should appear in the row right below it
+    When The user clicks on subproposal "5" of component "2" of MIP "4"
+    Then The view page corresponding to subproposal "5" of component "2" of MIP "4" should open
+    And The open page should contain the name of the sproposal "5" of component "2" of MIP "4" as main header
+
+#  Dark mode
+ Scenario: SubProposals open on MIPS where they are present
+    Given The user opens the main page
+    And Dark mode is toggled
+    When The user clicks dropdown icon on the row corresponding to MIP "4"
+    And The user opens the sub-proposals menu of component "2" of MIP "4"
+    Then The list of subproposals belonging to component "2" of MIP "4" should appear in the row right below it
+    When The user clicks on subproposal "5" of component "2" of MIP "4"
+    Then The view page corresponding to subproposal "5" of component "2" of MIP "4" should open
+    And The open page should contain the name of the sproposal "5" of component "2" of MIP "4" as main header
+
+#  Spanish
+ Scenario: SubProposals open on MIPS where they are present
+    Given The user opens the main page
+    And The user selects "Spanish" language
+    When The user clicks dropdown icon on the row corresponding to MIP "4"
+    And The user opens the sub-proposals menu of component "2" of MIP "4"
+    Then The list of subproposals belonging to component "2" of MIP "4" should appear in the row right below it
+    When The user clicks on subproposal "5" of component "2" of MIP "4"
+    Then The view page corresponding to subproposal "5" of component "2" of MIP "4" should open
+    And The open page should contain the name of the sproposal "5" of component "2" of MIP "4" as main header
--- frontend/cypress/integration/cucumber/normal-view-part-2/normal-view-part-2.js
+++ frontend/cypress/integration/cucumber/normal-view-part-2/normal-view-part-2.js
@@ -1,14 +1,13 @@
-
 // Component opens on MIPS where they are present
 Given("The user opens the main page", () => {
   cy.visit("");
 });
 
-And("Dark mode is toggled", () => {
+Given("Dark mode is toggled", () => {
   cy.get("div.darkModeToggler").click();
 });
 
-And("The user selects {string} language", (language) => {
+Given("The user selects {string} language", (language) => {
   cy.get("a.language-menu").click();
   cy.get("div.language-menu")
     .find("app-menu")
@@ -37,7 +36,7 @@ Then("The view page corresponding to MIP {string} should open", (mipNumber) => {
   });
 });
 
-And("The open page should contain the title of MIP {string}", (mipNumber) => {
+Then("The open page should contain the title of MIP {string}", (mipNumber) => {
   cy.get("@title").then((title) => {
     const mipName = title;
     console.log(mipName);
@@ -45,85 +44,105 @@ And("The open page should contain the title of MIP {string}", (mipNumber) => {
   });
 });
 
-// Component opens on MIPS where they are present
+When(
+  "The user clicks dropdown icon on the row corresponding to MIP {string}",
+  (mipNumber) => {
+    cy.get("tr[role=row].maker-element-row")
+      .filter(`:contains(MIP${mipNumber})`)
+      .find(".mat-button-wrapper > .arrow-wrapper")
+      .eq(0)
+      .click();
+  }
+);
 
-// Given("The user opens the main page", () => {
-//   cy.visit("");
-// });
+Then(
+  "The component {string} belonging to MIP {string} should appear in the row right below it",
+  (componentNumber, mipNumber) => {
+    cy.get("tr[role=row].maker-element-row").each((el, index) => {
+      if (el.text().includes(`MIP${mipNumber}`)) {
+        cy.get("tr[role=row].maker-detail-row")
+          .eq(index)
+          .find(`.subsetColumn:contains(MIP${mipNumber}c${componentNumber})`)
+          .should("exist");
+      }
+    });
+  }
+);
 
-// When("The user clicks the row corresponding to MIP {string}", (mipNumber) => {
-//   cy.get("tr[data-cy=search-result]")
-//     .filter(`:contains(MIP${mipNumber})`)
-//     .as("targetRow");
-//   cy.get("@targetRow")
-//     .find("td")
-//     .eq(1)
-//     .invoke("text")
-//     .then((text) => {
-//       cy.wrap(text).as("title"); // title of the mip
-//     });
-//   cy.get("@targetRow").click();
-// });
+When(
+  "The user opens the sub-proposals menu of component {string} of MIP {string}",
+  (component, mipNumber) => {
+    cy.get("tr[role=row]").each((el, index) => {
+      if (el.text().includes(`MIP${mipNumber}`)) {
+        cy.get("tr[role=row]")
+          .eq(index + 1)
+          .find(
+            `tr[role=row] > td[role=gridcell]:contains(MIP${mipNumber}c${component})`
+          )
+          .as("currentComponent");
+        cy.get("@currentComponent")
+          .find("div.arrow-wrapper")
+          .invoke("attr", "class")
+          .then((classes) => {
+            if (classes.indexOf("rotated") === -1)
+              // make sure it's not already open
+              cy.get("@currentComponent").click();
+          });
+      }
+    });
+  }
+);
 
-// Then("The view page corresponding to MIP {string} should open", (mipNumber) => {
-//   cy.location().then((loc) => {
-//     cy.wrap(loc.pathname).should("eq", `/mips/details/MIP${mipNumber}`);
-//   });
-// });
+Then(
+  "The list of subproposals belonging to component {string} of MIP {string} should appear in the row right below it",
+  (componentNumber, mipNumber) => {
+    cy.get("tr[data-cy=search-result].maker-element-row").each((el, index) => {
+      if (el.text().includes(`MIP${mipNumber}`)) {
+        cy.get("tr[role=row].maker-detail-row").eq(index).as("componentList");
+        cy.get("@componentList")
+          .find("tr[role=row].maker-element-subset-row > .subsetColumn")
+          .each((component, componentIndex) => {
+            if (
+              component.text().includes(`MIP${mipNumber}c${componentNumber}`)
+            ) {
+              cy.get("@componentList")
+                .find("tr[role=row].maker-detail-subset-row")
+                .eq(componentIndex)
+                .find("app-list-subproposal")
+                .should("exist");
+            }
+          });
+      }
+    });
+  }
+);
 
-// And("The open page should contain the title of MIP {string}", (mipNumber) => {
-//   cy.get("@title").then((title) => {
-//     const mipName = title;
-//     console.log(mipName);
-//     cy.get("app-detail-content").find("span.title").contains(mipName);
-//   });
-// });
+When(
+  "The user clicks on subproposal {string} of component {string} of MIP {string}",
+  (subproposalNumber, componentNumber, mipNumber) => {
+    cy.get("tr[data-cy=subporposal-row]")
+      .contains(`MIP${mipNumber}c${componentNumber}-SP${subproposalNumber}`)
+      .click();
+  }
+);
 
-// When(
-//   "The user clicks dropdown icon on the row corresponding to MIP {string}",
-//   () => {
-//     cy.get("tr[data-cy=search-result]")
-//       .filter(`:contains(MIP${mipNumber})`)
-//       .find(".mat-button-wrapper > .arrow-wrapper")
-//       .click();
-//   }
-// );
+Then(
+  "The view page corresponding to subproposal {string} of component {string} of MIP {string} should open",
+  (subproposalNumber, componentNumber, mipNumber) => {
+    cy.location().then((loc) => {
+      cy.wrap(loc.pathname).should(
+        "eq",
+        `/mips/details/MIP${mipNumber}c${componentNumber}SP${subproposalNumber}`
+      );
+    });
+  }
+);
 
-// Then("The components belonging to MIP {string} should appear", (mipNumber) => {
-//   cy.get("tr[role=row]").each((el, index) => {
-//     if (el.contains(`MIP${mipNumber}`))
-//       cy.wrap(index).as("mipComponentContainerIndex");
-//   });
-//   cy.get("tr[role=row]")
-//     .eq(cy.get("@mipComponentContainerIndex"))
-//     .invoke("attr", "data-cy")
-//     .should("not.equal", "search-result");
-// });
-
-// When(
-//   "The user opens the sub-proposals menu of component {string} of MIP {string}",
-//   (component, mipNumber) => {
-//     cy.get("tr[role=row]").each((el, index) => {
-//       if (el.contains(`MIP${mipNumber}`))
-//         cy.wrap(index).as("mipComponentContainerIndex");
-//     });
-//     cy.get("tr[role=row]")
-//       .eq(cy.get("@mipComponentContainerIndex"))
-//       .find(`:contains(${component})`)
-//       .as("currentComponent");
-//     cy.get("@currentComponent").then((mipComponent) => {
-//       mipComponent
-//         .find(".mat-button-wrapper > .arrow-wrapper > span > div")
-//         .invoke("attr", "class")
-//         .then((classes) => {
-//           if (classes.indexOf("rotated") === -1)
-//             cy.get("@currentComponent").click();
-//         });
-//     });
-//   }
-// );
-
-// Then(
-//   "The sub-proposals of component {string} of MIP {string} should appear",
-//   () => {}
-// );
+Then(
+  "The open page should contain the name of the sproposal {string} of component {string} of MIP {string} as main header",
+  (subproposalNumber, componentNumber, mipNumber) => {
+    cy.get("app-detail-content")
+      .find("span.title-bold")
+      .contains(`MIP${mipNumber}c${componentNumber}-SP${subproposalNumber}`);
+  }
+);
