01cf673d1ef4063bea3ce80af6d05d695fe07bb8
Daniel Vega <dvegap95@gmail.com>
2022-08-24 23:09:58 -0400

    feat: implementing menu interaction tests with cucumber

--- frontend/cypress/integration/common/generic.js
+++ frontend/cypress/integration/common/generic.js
@@ -6,6 +6,10 @@ And('{string} ms are past',(ms)=>{
     cy.wait(+ms);
 })
 
-And('The page is reloaded',(ms)=>{
+And('The page is reloaded',()=>{
     cy.reload();
+})
+
+And('The user opens the main page',()=>{
+    cy.visit('');
 })
