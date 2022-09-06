3567fb92f6e754ac480606ededede8a292089f01
Daniel Vega <dvegap95@gmail.com>
2022-09-05 22:48:11 -0400

    feat: moving multiquery view snapshots to headless (plain cypress specs)

--- frontend/cypress/integration/screenshots/multi-queries-view.spec.js
+++ frontend/cypress/integration/screenshots/multi-queries-view.spec.js
@@ -1,13 +1,24 @@
 /// <reference types="cypress" />
 
+const { fakeMenu } = require("../../helpers/fake-menu");
+const { fakeMips } = require("../../helpers/fake-mip");
+const { fakeMip } = require("../../helpers/fake-mip-details");
+const { fakeNews } = require("../../helpers/fake-news");
+const { fakeVars } = require("../../helpers/fake-vars");
+
 describe("Multi queries View", () => {
   before(() => {
+    fakeVars();
+    fakeNews();
+    fakeMips();
+    fakeMip();
+    fakeMenu();
     const search = {
-      customViewName: 'Dai Foundation Core Unit (DAIF-001) Subproposals',
-      '_Active Subproposals': '$AND(#active,#cu-daif-001)',
-      _Archive: '$AND(NOT(#active),#cu-daif-001)',
-      shouldBeExpandedMultiQuery: 'true',
-      hideParents: 'false',
+      customViewName: "Dai Foundation Core Unit (DAIF-001) Subproposals",
+      "_Active Subproposals": "$AND(#active,#cu-daif-001)",
+      _Archive: "$AND(NOT(#active),#cu-daif-001)",
+      shouldBeExpandedMultiQuery: "true",
+      hideParents: "false",
     };
     const params = new URLSearchParams(search);
     const url = `/mips/list?${params.toString()}`;
@@ -15,21 +26,24 @@ describe("Multi queries View", () => {
     cy.visit(url);
   });
 
-  it('Entire View', () => {
-    Cypress.$('app-button-top').remove();
-    cy.testScreenshot(null, 'multi-queries-view/entire-view');
+  it("Entire View", () => {
+    Cypress.$("app-button-top").remove();
+    cy.testScreenshot(null, "multi-queries-view/entire-view");
   });
 
-  it('Title', () => {
-    cy.get('app-list-page .container').first().then($el => {
-      cy.testScreenshot($el, 'multi-queries-view/title')
-    })
+  it("Title", () => {
+    cy.get("app-list-page .container")
+      .first()
+      .then(($el) => {
+        cy.testScreenshot($el, "multi-queries-view/title");
+      });
   });
 
-  it('Table', () => {
-    cy.get('table').first().then($el => {
-      cy.testScreenshot($el, 'multi-queries-view/table')
-    })
+  it("Table", () => {
+    cy.get("table")
+      .first()
+      .then(($el) => {
+        cy.testScreenshot($el, "multi-queries-view/table");
+      });
   });
-
 });
