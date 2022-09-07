95a83281eff63145d14f9ddc35e2fbee1c04a677
Daniel Vega <dvegap95@gmail.com>
2022-09-07 16:41:39 -0400

    feat: moving details view snapshots to headless

--- frontend/cypress/integration/common/snapshoots.js
+++ frontend/cypress/integration/common/snapshoots.js
@@ -17,3 +17,16 @@ Then(
       });
   }
 );
+
+Then(
+  "{string} component with selector {string} opened by clicking {string} matches snapshot for image name {string}",
+  (_name, selector,clickSelector, imageName) => {
+    cy.get(clickSelector).first().click({force:true});
+    cy.wait(700);
+    cy.get(selector)
+      .first()
+      .then(($el) => {
+        cy.testScreenshot($el, imageName);
+       });
+  }
+);
