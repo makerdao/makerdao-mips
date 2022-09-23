639434df6a9e58d13b68001f7ff4636659ae0258
Daniel Vega <dvegap95@gmail.com>
2022-09-23 19:05:23 -0400

    fix: mocking dynamic data for regular-search feature

--- frontend/cypress/integration/cucumber/dollar-sign/dollar-sign.js
+++ frontend/cypress/integration/cucumber/dollar-sign/dollar-sign.js
@@ -21,16 +21,4 @@ Then('The found MIps should contain the tag {string}',(tag)=>{
   })
 })
 
-Then(
-  "The mips should be requested with the search criteria {string}",
-  (search) => {
-    cy.get("@MIPs").then((req) => {
-      cy.wrap(req.request.url).should(
-        "include",
-        // Hardcoded url encode since encodeURI will include dollar sign
-        `search=${search.replace(/\s/g, "%20").replace(/#/g, "%23")}` 
-      );
-    });
-  }
-);
 
