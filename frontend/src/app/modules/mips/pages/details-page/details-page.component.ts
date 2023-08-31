742383f764a2554492a6e07e0280b8c35f8b6342
hernandoagf <hernando.guzman93@gmail.com>
2023-08-31 12:47:02 -0300

    Check URLs without converting to lowercase

--- frontend/src/app/modules/mips/pages/details-page/details-page.component.ts
+++ frontend/src/app/modules/mips/pages/details-page/details-page.component.ts
@@ -72,14 +72,10 @@ export class DetailsPageComponent implements OnInit {
 
         const shouldUpdateUrl = this.urlService.getMdFromGithubUrl(url);
         const isUrlFromValidOrganization =
-          url
-            .toLowerCase()
-            .startsWith('https://github.com/makerdao/mips/blob/master') ||
-          url
-            .toLowerCase()
-            .startsWith(
-              'https://raw.githubusercontent.com/makerdao/mips/master'
-            );
+          url.startsWith('https://github.com/makerdao/mips/blob/master') ||
+          url.startsWith(
+            'https://raw.githubusercontent.com/makerdao/mips/master'
+          );
 
         if (shouldUpdateUrl) {
           this.router.navigateByUrl(this.urlService.transformLinkForMd(url));
