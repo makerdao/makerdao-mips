05288fa626b9fad1d9447a08d81611b4c26530f1
Simon KP <simon@techops.services>
2024-03-15 12:47:41 +1100

    dev

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://mips-api.makerdao.com',
-  apiUrl: 'http://backend:3000',
+  apiUrl: 'https://mips-api.makerdao.com',
   repoUrl: 'https://github.com/makerdao/mips/blob/master',
   feedBackFormUrl: 'https://formspree.io/f/xvodvpze',
   githubURL: 'https://github.com/',
